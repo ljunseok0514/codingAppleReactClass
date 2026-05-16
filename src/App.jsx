b4dd5c49e5c7bcf4784a8fa54470aa08197c90a0
e0129 <jo_ohyeon@naver.com>
2026-05-16 16:27:04 +0900

    260516 코딩애플 리액트 강의 시작

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let post = '강남우동맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','맛집추천','여자 옷 추천']);
+  let [따봉, 따봉변경] = useState(0);  
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <button onClick={() => {
+          let copy = [...글제목];
+          copy[0] ='여자코트 추천';
+          // 글제목[0] = '여자코트 추천';
+          글제목변경(copy);
+        }}>
+          글수정
+      </button>
+      <div className="list">
+        
+        <h4>{글제목[0]} <span onClick={()=> {따봉변경(따봉+1)}}>👍</span> {따봉}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
